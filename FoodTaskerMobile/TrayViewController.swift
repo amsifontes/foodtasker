f9c1f493f97150311a6b345f5afc58c325854347
anthony sifontes <asifontes@anthonys-MacBook-Pro.local>
2018-12-22 20:10:09 -0800

    add class for Tray View Controller

--- FoodTaskerMobile/TrayViewController.swift
+++ FoodTaskerMobile/TrayViewController.swift
@@ -1,31 +1,29 @@
 //
-//  RestaurantViewController.swift
+//  TrayViewController.swift
 //  FoodTaskerMobile
 //
-//  Created by anthony sifontes on 12/21/18.
+//  Created by anthony sifontes on 12/22/18.
 //  Copyright © 2018 anthony sifontes. All rights reserved.
 //
 
 import UIKit
 
-class RestaurantViewController: UIViewController {
+class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
         if self.revealViewController() != nil {
             menuBarButton.target = self.revealViewController()
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+
     }
 }
 
-extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
+extension TrayViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -33,8 +31,10 @@ extension RestaurantViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrayItemCell", for: indexPath)
+        
         return cell
     }
 }
