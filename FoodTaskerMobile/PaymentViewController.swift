6645f918547926548f22b74bec7b5ae669d6545b
anthony sifontes <asifontes@anthonys-MacBook-Pro.local>
2018-12-23 21:37:55 -0800

    add Stripe credit card layout

---
+++ FoodTaskerMobile/PaymentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by anthony sifontes on 12/23/18.
+//  Copyright © 2018 anthony sifontes. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class PaymentViewController: UIViewController {
+
+    @IBOutlet weak var cardTextField: STPPaymentCardTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
